3fc62a64a95bdc73f227b6e14719a019d1dd56ea
hya-cala <Rena.Yang39@gmail.com>
2022-08-03 21:47:16 -0400

    delete abundant folder

--- Front/src/App.js
+++ Front/src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import './bootstrap.css';
 
 class App extends Component {
+  state = { provider: null }
+  constructor(props) {
+    super(props);
+    console.log(window.aleereum)
+    this.setState({ provider: window.aleereum })
+    // console.log(window.ethereum);
+  }
+
   render() {
     return (
       <div className='App'>
